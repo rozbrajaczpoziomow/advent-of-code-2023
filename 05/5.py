d2b95f732e4307b7240ccdfb29aaf64999b39866
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2023-12-05 06:47:02 +0100

    Day 5

--- 05/5.py
+++ 05/5.py
@@ -7,13 +7,87 @@ from string import *
 
 def PART1(case):
 	s = 0
+	lines = []
 	for line in case.readlines():
-		pass
+		lines += [line]
+
+	seeds = [*map(int,lines[0].split(': ')[1].split(' '))]
+	mp = {}
+	for gay in (''.join(lines)).split('\n\n')[1:]:
+		print(gay)
+		header,*vals = gay.split('\n')
+		sts = []
+		sts += [*filter(lambda x:x!='',vals)]
+
+		# print('VEY GYA')
+
+		# print(sts)
+		# destRangeStart srcRangeStart rangeLen
+		mp.clear()
+		for l in sts:
+			destRangeStart, srcRangeStart, rangeLen = [*map(int, l.strip().split(' '))]
+			mp[range(srcRangeStart, srcRangeStart+rangeLen+1)] = destRangeStart - srcRangeStart
+
+		print(mp)
+
+		amp = []
+		for s in seeds:
+			mapped = s
+			for r in mp.keys():
+				if s in r:
+					mapped += mp[r]
+					print('found match for', s, mp[r], 'in range',r)
+					break
+			print(s, '>', mapped)
+			amp += [mapped]
+		seeds = amp
+		print('----------')
+	return min(seeds)
 
 def PART2(case):
 	s = 0
+	lines = []
 	for line in case.readlines():
-		pass
+		lines += [line]
+
+	sd = [*map(int,lines[0].split(': ')[1].split(' '))]
+	seeds = []
+	for (i, v) in enumerate(range(0, len(sd), 2)):
+		seeds += [*range(sd[v], sd[v]+sd[v+1]+1)]
+	print('done with seeds')
+
+	mp = {}
+	for gay in (''.join(lines)).split('\n\n')[1:]:
+		# print(gay)
+		header,*vals = gay.split('\n')
+		sts = []
+		sts += [*filter(lambda x:x!='',vals)]
+
+		# print('VEY GYA')
+
+		# print(sts)
+		# destRangeStart srcRangeStart rangeLen
+		mp.clear()
+		for l in sts:
+			destRangeStart, srcRangeStart, rangeLen = [*map(int, l.strip().split(' '))]
+			mp[(srcRangeStart, srcRangeStart+rangeLen+1)] = destRangeStart - srcRangeStart
+
+		# print(mp)
+
+		amp = []
+		for s in seeds:
+			mapped = s
+			for r in mp.keys():
+				if s >= r[0] and s < r[1]:
+					mapped += mp[r]
+					# print('found match for', s, mp[r], 'in range',r)
+					break
+			# print(s, '>', mapped)
+			amp += [mapped]
+		print('done iter')
+		seeds = amp
+		# print('----------')
+	return min(seeds)
 
 
 
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ now, I haven't had any practice with python for a while to expect some terrible 
 ```
       --------Part 1--------   --------Part 2--------
 Day       Time   Rank  Score       Time   Rank  Score
+  5   00:26:09   2335      0   00:43:47    599      0
   4   12:32:25  66407      0   12:40:19  53961      0
   3   03:15:25  16422      0   03:30:16  13423      0
   2   00:42:57   9255      0   00:45:22   8014      0
